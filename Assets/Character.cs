711e69031bf77ac1069967877106fc6fd268ecca
sjf25 <sohaibjf@gmail.com>
2016-09-10 14:09:06 -0500

    Add files via upload

---
+++ Assets/Character.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	public KeyCode up, down, left, right, shoot;
+	public bool goingLeft, goingRight, goingUp, goingDown;
+	public int health;
+	public bool facingRight;
+	// velocity per frame
+	public float velocity = 1F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = this.transform.position;
+		if (Input.GetKeyDown (left)) {
+			goingLeft = true;
+		} else if (Input.GetKeyUp (left)) {
+			goingRight = false;
+		}
+
+		if (goingLeft) {
+			newPos.x -= velocity * Time.deltaTime;
+			//newPos.x -= .1F;
+		}
+		this.transform.position = newPos;
+	}
+
+	public bool isDead() {
+		return health <= 0;
+	}
+}
